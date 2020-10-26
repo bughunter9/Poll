18c65e3fcb59666d94f2f9beacf5d8fbf00d499d
MANROOP PARMAR <manroopsingh120@gmail.com>
2020-10-26 23:52:39 +0530

    Fixed

--- dope/settings.py
+++ dope/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nqzlk@&s&yfv%le869t#+j+5axm0x4nj42@!8%zu84tk)o$^7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
